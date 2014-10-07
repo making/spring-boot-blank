469d5a9868fb91978e121b24c338f308cad81676
Toshiaki Maki <makingx@gmail.com>
2014-10-08 02:33:40 +0900

    update script

--- create-maven-archetype.sh
+++ create-maven-archetype.sh
@@ -13,6 +13,7 @@ mvn archetype:create-from-project
 
 pushd target/generated-sources/archetype
 sed -i -e "/$KEYWORD/d" target/classes/archetype-resources/pom.xml
+sed -i -e "s/xxxxxx\.yyyyyy\.zzzzzz/\${package}/g" target/classes/archetype-resources/pom.xml
 sed -i -e "s/xxxxxx\.yyyyyy\.zzzzzz/am.ik.archetype/g" pom.xml
 sed -i -e "s/projectName/spring-boot-blank/g" pom.xml
 sed -i -e "s/<scm>/<distributionManagement><snapshotRepository><id>ossrh<\/id><url>https:\/\/oss.sonatype.org\/content\/repositories\/snapshots<\/url><\/snapshotRepository><repository><id>ossrh<\/id><url>https:\/\/oss.sonatype.org\/service\/local\/staging\/deploy\/maven2\/<\/url><\/repository><\/distributionManagement><scm>/g" pom.xml
