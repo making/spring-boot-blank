7ac10358f48648b3b4d99821961dd91c396eadc6
Toshiaki Maki <makingx@gmail.com>
2014-12-12 09:40:33 +0900

    support @SpringBootApplication

--- src/main/java/xxxxxx/yyyyyy/zzzzzz/App.java
+++ src/main/java/xxxxxx/yyyyyy/zzzzzz/App.java
@@ -1,11 +1,9 @@
 package xxxxxx.yyyyyy.zzzzzz;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
