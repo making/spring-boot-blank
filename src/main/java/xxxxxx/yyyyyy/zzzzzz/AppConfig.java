2ae98c03265abc878b6f01467c90dfd0920846b9
Toshiaki Maki <makingx@gmail.com>
2015-10-17 17:40:03 +0900

    Polish

--- src/main/java/xxxxxx/yyyyyy/zzzzzz/AppConfig.java
+++ src/main/java/xxxxxx/yyyyyy/zzzzzz/AppConfig.java
@@ -4,8 +4,10 @@ import net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
@@ -15,13 +17,20 @@ public class AppConfig {
     DataSourceProperties dataSourceProperties;
 
     @Bean
-    DataSource dataSource() {
+    @ConfigurationProperties(prefix = DataSourceProperties.PREFIX)
+    DataSource realDataSource() {
         DataSource dataSource = DataSourceBuilder
                 .create(this.dataSourceProperties.getClassLoader())
                 .url(this.dataSourceProperties.getUrl())
                 .username(this.dataSourceProperties.getUsername())
                 .password(this.dataSourceProperties.getPassword())
                 .build();
-        return new DataSourceSpy(dataSource);
+        return dataSource;
+    }
+
+    @Bean
+    @Primary
+    DataSource dataSource() {
+        return new DataSourceSpy(realDataSource());
     }
 }
